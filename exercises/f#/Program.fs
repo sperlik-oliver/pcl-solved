a11ef97b578b67ac0a8d317bde5277380642a40b
sperlik-oliver <osp@whiteawaygroup.com>
2023-05-29 15:58:24 +0200

    f# done

--- exercises/f#/Program.fs
+++ exercises/f#/Program.fs
@@ -1,2 +1,280 @@
-﻿printfn "Hello from F#"
+﻿// 2.1a
+
+let vowelToUpper (c): string = 
+    match c with
+    | "a" -> "A"
+    | "e" -> "E"
+    | "i" -> "I"
+    | "o" -> "O"
+    | "u" -> "U"
+    | _ -> c
+
+let x = vowelToUpper "x"  
+let a = vowelToUpper "a"
+
+// 2.1b
+
+let rec convertVowelsToUpper (str: string) : string =
+    match str with
+    | "" -> ""
+    | _ ->
+        let firstChar = str[0..0]
+        let rest = str.[1..]
+        let convertedChar = vowelToUpper firstChar
+        let convertedRest = convertVowelsToUpper rest
+        convertedChar + convertedRest
+
+let picovina = convertVowelsToUpper "picovina"
+
+// 2.2a
+
+let rec pmLength list =
+    match list with
+    | [] -> 0                     
+    | _ :: tail -> 1 + pmLength tail 
+
+let result = pmLength [1;2;3;4]
+
+// 2.2b
+
+let rec pclSum list = 
+    match list with
+    | [] -> 0
+    | x :: tail -> x + pclSum tail
+
+let result2 = pclSum [1;2;3;4]
+
+// 2.3
+
+let takeSome n ls = 
+    match ls with
+    | [] -> []
+    | _ -> ls[0..n-1]
+
+let result3 = takeSome 3 [1;2;3;4;5]
+
+// 2.4a
+
+let rec pclFold func acc lst =
+    match lst with
+    | [] -> acc
+    | x :: tail -> pclFold func (func acc x) tail
+
+// 2.4b
+
+let pclSumWithFold lst = pclFold (+) 0 lst
+
+let result4 = pclSumWithFold [1;2;3;4;5]
+
+// 2.5a
+
+
+let rec pclFoldBack func acc lst =
+    match lst with
+    | [] -> acc
+    | x :: tail -> func x (pclFoldBack func acc tail)
+
+// 2.5b
+
+let pclSumWithFoldBack lst =
+    pclFoldBack (+) 0 lst
+
+// 2.5c
+
+// results can be different between pclFold and pclFoldBack in non-associative scenarios like subtraction
+
+let result5 = pclFold (-) 0 [1;2;3]
+let result6 = pclFoldBack (-) 0 [1;2;3]
+
+// 2.6
+
+let rec pclIncList lst =
+    match lst with
+    | [] -> []
+    | x :: tail -> (x + 1) :: pclIncList tail
+
+let result7 = pclIncList [1;2;3;4]
+
+// 2.7a
+
+let rec pclMap fn lst =
+    match lst with
+    | [] -> []
+    | x :: tail -> fn x :: pclMap fn tail
+
+// 2.7b
+
+let pclIncListWithMap lst = pclMap (fun x -> x + 1) lst
+
+let result8 = pclIncListWithMap [1;2;3;4;5]
+
+// 2.8a
+
+let rec pclFilter predicate lst =
+    match lst with
+    | [] -> []
+    | x :: tail -> if (predicate x) then x :: pclFilter predicate tail else pclFilter predicate tail
+
+
+// 2.8b
+
+let rec pclEven x = x % 2 = 0
+
+// 2.8c
+
+let result9 = pclFilter pclEven [1;2;3;4;5;6;7;8;9;10]
+
+// for some reason there are 2 sets
+// of exercises that start with number 2 (i.e. 2.1)
+// but the set that comes later in the exercise list i have given
+// has been skipped here because they are super simple
+
+// 3.1
+
+let countVowel vowelCount vowel = 
+    let (a, e, i, o, u) = vowelCount
+    match vowel with
+        | 'a' | 'A'-> (a + 1, e, i, o, u)
+        | 'e' | 'E' -> (a, e + 1, i, o, u)
+        | 'i' | 'I' -> (a, e, i + 1, o, u)
+        | 'o' | 'O' -> (a, e, i, o + 1, u)
+        | 'u' | 'U' -> (a, e, i, o, u + 1)
+        | _ -> vowelCount
+
+let countNumOfVowels str = List.fold countVowel (0, 0, 0, 0, 0) (Seq.toList str)
+
+let result10 = countNumOfVowels "Higher-order functions can take and return functions of any order"
+
+// 3.2
+
+let isPrime n =
+    let rec isDivisibleBy k d =
+        if d * d > k then
+            true
+        elif k % d = 0 then
+            false
+        else
+            isDivisibleBy k (d + 1)
+    n > 1 && isDivisibleBy n 2
+
+let primesUpTo n =
+    let lst = [2..n]
+    List.filter isPrime lst
+
+let result11 = primesUpTo 10
+
+// 3.3
+
+let rec pclFib n =
+    match n with
+    | 0 -> 0
+    | 1 -> 1
+    | _ -> pclFib (n - 1) + pclFib (n - 2)
+
+let result12 = pclFib 8
+
+
+// 3.4a
+
+let doubleNum x = x * 2
+
+let sqrNum x = x * x
+
+// 3.4b
+
+let pclQuad x = doubleNum(doubleNum x)
+
+// 3.4c
+
+let pclFourth x = sqrNum(sqrNum x)
+
+// 4.1 & 5.1
+
+type Shape = | Rectangle | Triangle
+
+type PclShape = { shape: Shape; a: float; b: float }
+
+let pclArea x = if (x.shape = Rectangle) then x.a * x.b  else ((x.a * x.b)/2.0) 
+let pclPerimeter x = if (x.shape = Rectangle) then x.a*2.0 + x.b*2.0 else x.a*2.0 + x.b 
+    
+let rect: PclShape = { 
+    shape = Rectangle
+    a = 4
+    b = 3
+}
+
+let triangle: PclShape = {
+    shape = Triangle
+    a = 5
+    b = 2137
+}
+
+let rectangleArea = pclArea rect
+let triangleArea = pclArea triangle
+let rectanglePerimeter = pclPerimeter rect
+let trianglePerimeter = pclPerimeter triangle
+
+// 5.2
+
+let fibonacci n =
+    let rec fibonacciTail n a b =
+        match n with
+        | 0 -> a
+        | _ -> fibonacciTail (n - 1) b (a + b)
+    
+    fibonacciTail n 0 1
+let result13 = fibonacci 8
+
+// 5.3
+
+let rec factorial n cont =
+    match n with
+    | 0 -> cont 1
+    | _ -> factorial (n - 1) (fun x -> cont (n * x))
+
+let printFactorial result = printfn "Factorial %d" result
+
+factorial 5 printFactorial
+
+// 6a
+
+type IntegerTree =
+    | Leaf of int
+    | Node of IntegerTree * IntegerTree
+
+let rec sumIntegerTree tree =
+    match tree with
+    | Leaf value -> value
+    | Node (left, right) -> sumIntegerTree left + sumIntegerTree right
+
+let tree1 = Node (Leaf 1, Node (Leaf 2, Leaf 3))
+let result15 = sumIntegerTree tree1  // Result: 6
+
+let tree2 = Node (Leaf 4, Node (Leaf 5, Leaf 6))
+let result16 = sumIntegerTree tree2  // Result: 15
+
+// 6b
+
+type WordLetterCount =
+    { WordCount : int
+      LetterCount : int }
+
+let countWordnLetter (str: string) =
+    let wordCount = str.Split [| ' ' |]
+    let letterCount = wordCount |> Array.sumBy (fun w -> w.Length)
+    { WordCount = wordCount.Length; LetterCount = letterCount }
+
+// 7
+
+let isLeapYear year =
+    year % 4 = 0 && (year % 100 <> 0 || year % 400 = 0)
+
+let addDays result year = if isLeapYear year then result+366 else result+365
+
+let daysToEndYear year = List.fold addDays 0 [1 .. year]
+
+let result17 = daysToEndYear 1
+
+
+
 
--- test-exam/f#/Program.fs
+++ test-exam/f#/Program.fs
@@ -1,4 +1,162 @@
-﻿
+﻿// 1a
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+ let sqrOnlyFirst ls =
+    match ls with
+        | hd :: _ -> hd * hd
+
+let result1 = sqrOnlyFirst [2;4;6] // 4
+let result2 = sqrOnlyFirst [] // The match ses were incomplete, stopped due to error
+
+
+ let sqrOnlyFirstFixed ls =
+    match ls with
+      | [] -> 0
+      | hd :: _ -> hd * hd
+
+let result3 = sqrOnlyFirstFixed [2;4;6]
+let result4 = sqrOnlyFirstFixed []
+
+// 1b
+
+let rec stringToList str =
+    match str with
+    | "" -> []
+    | _ -> 
+        let firstChar = char(str[0..0]) // i dont know if that counts as built in conversion, is there any way to do it without literally using anything built-in??
+        firstChar :: stringToList str[1..]
+
+let result5 = stringToList "Jebem Boha"
+
+
+// 2a
+
+type Vector = {
+    a: int;
+    b: int;
+    c: int;
+    d: int;
+    e: int;
+}
+
+// 2b
+
+let vecLen vector = vector.a + vector.b + ( vector.c) + vector.d + vector.e
+
+// 2c
+
+let vecAdd vectorA vectorB = vecLen vectorA + vecLen vectorB
+
+
+// 3a
+
+let rec rerun s n =
+    match s, n with
+    | _, 0 -> ""
+    | _, _ -> s + rerun s (n-1)
+
+
+let result6 = rerun "pica" 3
+
+
+// 3b
+
+let rec rt s n acc = 
+    match s, n with
+    | _, 0 -> acc
+    | _, _ -> rt s (n-1) (s+acc)
+
+let rerunTail s n = rt s n ""
+
+let result7 = rerunTail "kokot" 3
+
+// 4
+
+let f1 i j k =
+    seq {
+         for x in [0 .. i] do
+             for y in [0 .. j] do 
+                if x+y < k then yield (x,y)
+}
+let f2 f p sq =
+    seq {
+         for x in sq do
+            if p x then yield f x
+}
+let f3 g sq =
+    seq {
+         for s in sq do
+            yield! g s
+}
+
+// 4a
+
+let result8 = List.ofSeq(f1 2 2 3) // [(0, 0); (0, 1); (0, 2); (1, 0); (1, 1); (2, 0)]
+
+// 4b
+// wtf is happening here i have no idea
+let f2seq f p sq =
+    sq
+    |> Seq.filter p
+    |> Seq.map f
+
+// 5a
+
+type expr = | Const of int
+            | BinOpr of expr * string * expr
+
+let expr1 = Const(1)
+let expr2 = BinOpr(Const(1), "+", BinOpr(Const(1), "+", Const(2)))
+let expr3 = BinOpr(Const(3), "-", Const(2))
+
+// 5b
+
+let rec toString expr =
+        match expr with
+        | Const x -> string x
+        | BinOpr (x, op, y) -> sprintf "(%s %s %s)" (toString x) op (toString y)
+
+let result10 = toString expr2
+
+// 6
+
+type Product = {
+    name: string;
+    quantity: int;
+}
+
+let g2gAgent: MailboxProcessor<Product> =
+    MailboxProcessor.Start(fun inbox ->
+        let rec loop =
+            async {
+                let! order = inbox.Receive()
+
+                printfn "%s %d" order.name order.quantity 
+                printfn "\n"
+                return! loop
+            }
+
+        loop)
+
+let order1 = g2gAgent.Post({ 
+    name = "Picovina" 
+    quantity = 69420
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
